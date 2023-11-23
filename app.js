a496b20db48cff091d84bb49baa71cf1edba61cd
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2023-11-23 17:55:47 +0530

    Done

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // Progression 1: create a function and fetch the api using axios
+
+const apikey = '54e107ff1f2543a9b313e4f34c06f8ef';
+const url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + apikey;
+
+// axios.get(url).then(response =>{
+//     localStorage.setItem("article", JSON.stringify(response.data))
+// })
+
+
+function getData() {
+    axios.get(url).then(response =>{
+            let data = response.data.articles
+
+            let output = ""
+            for (let i = 0; i < data.length; i++) {
+                output+= `<div id="card">
+                <h3>${data[i].title}</h3>
+                <img src="${data[i].image}" alt="">
+                <p>${data[i].description}
+                </p>
+              </div>`
+            }
+            document.getElementById("news").innerHTML = output
+        })
+
+}
+
+getData()
